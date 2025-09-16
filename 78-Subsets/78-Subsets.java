ec77c0f5cbc9cba8eb6d295b3b7b1d988471d462
Rachit Gupta <160449023+rachit-gupta2564@users.noreply.github.com>
2025-09-16 23:00:04 +0530

    File updated at 16/09/2025, 23:00:01

--- 78-Subsets/78-Subsets.java
+++ 78-Subsets/78-Subsets.java
@@ -1,14 +1,9 @@
-// Last updated: 16/09/2025, 22:58:56
+// Last updated: 16/09/2025, 23:00:01
 class Solution {
     public ListNode deleteDuplicates(ListNode head) {
         if (head == null) return null;
-        if (head.next != null && head.val == head.next.val) {
-            int v = head.val;
-            while (head != null && head.val == v) head = head.next;
-            return deleteDuplicates(head);
-        } else {
-            head.next = deleteDuplicates(head.next);
-            return head;
-        }
+        head.next = deleteDuplicates(head.next);
+        if (head.next != null && head.val == head.next.val) head.next = head.next.next;
+        return head;
     }
 }
