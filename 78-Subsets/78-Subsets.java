9bbd7934fe2516e0ed8741da0c1177a3757cde38
Rachit Gupta <160449023+rachit-gupta2564@users.noreply.github.com>
2025-09-16 22:55:28 +0530

    File updated at 16/09/2025, 22:55:25

--- 78-Subsets/78-Subsets.java
+++ 78-Subsets/78-Subsets.java
@@ -1,16 +1,33 @@
-// Last updated: 16/09/2025, 22:54:16
+// Last updated: 16/09/2025, 22:55:25
 import java.util.*;
 
 class Solution {
-    public List<List<Integer>> subsets(int[] nums) {
-        int n = nums.length;
-        int total = 1 << n;
-        List<List<Integer>> res = new ArrayList<>(total);
-        for (int mask = 0; mask < total; mask++) {
-            List<Integer> cur = new ArrayList<>();
-            for (int j = 0; j < n; j++) if ((mask & (1 << j)) != 0) cur.add(nums[j]);
-            res.add(cur);
+    private static class Node { int r, c, idx; long mask; Node(int R,int C,int I,long M){r=R;c=C;idx=I;mask=M;} }
+    public boolean exist(char[][] board, String word) {
+        int m = board.length, n = board[0].length;
+        int L = word.length();
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (board[i][j] != word.charAt(0)) continue;
+                Deque<Node> st = new ArrayDeque<>();
+                long startMask = 1L << (i * n + j);
+                st.push(new Node(i, j, 1, startMask));
+                while (!st.isEmpty()) {
+                    Node cur = st.pop();
+                    if (cur.idx == L) return true;
+                    int[][] d = {{1,0},{-1,0},{0,1},{0,-1}};
+                    for (int[] dir : d) {
+                        int nr = cur.r + dir[0], nc = cur.c + dir[1];
+                        if (nr < 0 || nc < 0 || nr >= m || nc >= n) continue;
+                        int bit = nr * n + nc;
+                        if (((cur.mask >> bit) & 1L) == 1L) continue;
+                        if (board[nr][nc] != word.charAt(cur.idx)) continue;
+                        long nm = cur.mask | (1L << bit);
+                        st.push(new Node(nr, nc, cur.idx + 1, nm));
+                    }
+                }
+            }
         }
-        return res;
+        return false;
     }
 }
