b0110a92af91ff843278b744d2a60b1a06a8b041
Rachit Gupta <160449023+rachit-gupta2564@users.noreply.github.com>
2025-09-20 00:33:03 +0530

    Create 115-DistinctSubsequences

---
+++ 115-DistinctSubsequences/115-DistinctSubsequences.java
@@ -0,0 +1,21 @@
+// Last updated: 20/09/2025, 00:33:02
+class Solution {
+    public int numDistinct(String s, String t) {
+        int n = s.length();
+        int m = t.length();
+        // use long to be safe for intermediate sums; final answer fits in 32-bit per problem statement
+        long[] dp = new long[m + 1];
+        dp[0] = 1; // one way to form empty t
+
+        for (int i = 1; i <= n; i++) {
+            // iterate j from m down to 1 to avoid overwriting dp[j-1] needed for this row
+            for (int j = m; j >= 1; j--) {
+                if (s.charAt(i - 1) == t.charAt(j - 1)) {
+                    dp[j] += dp[j - 1];
+                }
+            }
+        }
+
+        return (int) dp[m];
+    }
+}
