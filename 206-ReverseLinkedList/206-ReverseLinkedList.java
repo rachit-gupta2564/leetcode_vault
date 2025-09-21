3de29890cead8e7cec9d241eb3dccceeee8b51da
Rachit Gupta <160449023+rachit-gupta2564@users.noreply.github.com>
2025-09-21 16:58:45 +0530

    File updated at 21/09/2025, 16:58:44

--- 206-ReverseLinkedList/206-ReverseLinkedList.java
+++ 206-ReverseLinkedList/206-ReverseLinkedList.java
@@ -1,37 +1,13 @@
-// Last updated: 21/09/2025, 16:57:57
+// Last updated: 21/09/2025, 16:58:44
 import java.util.*;
 
 class Solution {
-    public List<List<Integer>> getSkyline(int[][] buildings) {
-        List<int[]> events = new ArrayList<>();
-        for (int[] b : buildings) {
-            events.add(new int[]{b[0], -b[2]}); // entering, height negative
-            events.add(new int[]{b[1], b[2]});  // leaving, height positive
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; ++i) {
+            if (map.containsKey(nums[i]) && i - map.get(nums[i]) <= k) return true;
+            map.put(nums[i], i);
         }
-        events.sort((a, b) -> {
-            if (a[0] != b[0]) return a[0] - b[0];
-            return a[1] - b[1];
-        });
-
-        TreeMap<Integer, Integer> map = new TreeMap<>(Collections.reverseOrder());
-        map.put(0, 1);
-        int prev = 0;
-        List<List<Integer>> res = new ArrayList<>();
-
-        for (int[] e : events) {
-            int x = e[0], h = e[1];
-            if (h < 0) { // entering
-                map.put(-h, map.getOrDefault(-h, 0) + 1);
-            } else { // leaving
-                map.put(h, map.get(h) - 1);
-                if (map.get(h) == 0) map.remove(h);
-            }
-            int cur = map.firstKey();
-            if (cur != prev) {
-                res.add(Arrays.asList(x, cur));
-                prev = cur;
-            }
-        }
-        return res;
+        return false;
     }
 }
