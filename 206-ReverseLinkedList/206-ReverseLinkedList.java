2518a53ff2065f084349c0b846c9205c17167ea6
Rachit Gupta <160449023+rachit-gupta2564@users.noreply.github.com>
2025-09-21 16:55:59 +0530

    File updated at 21/09/2025, 16:55:58

--- 206-ReverseLinkedList/206-ReverseLinkedList.java
+++ 206-ReverseLinkedList/206-ReverseLinkedList.java
@@ -1,17 +1,35 @@
-// Last updated: 21/09/2025, 16:54:27
+// Last updated: 21/09/2025, 16:55:58
+import java.util.*;
+
 class Solution {
-    public int rob(int[] nums) {
+    private final Random rand = new Random();
+    public int findKthLargest(int[] nums, int k) {
         int n = nums.length;
-        if (n == 1) return nums[0];
-        return Math.max(robLinear(nums, 0, n - 2), robLinear(nums, 1, n - 1));
-    }
-    private int robLinear(int[] a, int l, int r) {
-        int prev = 0, cur = 0;
-        for (int i = l; i <= r; ++i) {
-            int nxt = Math.max(cur, prev + a[i]);
-            prev = cur;
-            cur = nxt;
+        int target = n - k;
+        int lo = 0, hi = n - 1;
+        while (lo <= hi) {
+            int pivotIndex = lo + rand.nextInt(hi - lo + 1);
+            int pos = partition(nums, lo, hi, pivotIndex);
+            if (pos == target) return nums[pos];
+            else if (pos < target) lo = pos + 1;
+            else hi = pos - 1;
         }
-        return cur;
+        return -1;
+    }
+    private int partition(int[] a, int l, int r, int pivotIdx) {
+        int pv = a[pivotIdx];
+        swap(a, pivotIdx, r);
+        int store = l;
+        for (int i = l; i < r; ++i) {
+            if (a[i] < pv) {
+                swap(a, store, i);
+                store++;
+            }
+        }
+        swap(a, store, r);
+        return store;
+    }
+    private void swap(int[] a, int i, int j) {
+        int t = a[i]; a[i] = a[j]; a[j] = t;
     }
 }
