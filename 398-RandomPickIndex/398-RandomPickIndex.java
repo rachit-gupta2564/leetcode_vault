337e76c5f4d5a4c74649dfa9441284d946e213f6
Rachit Gupta <160449023+rachit-gupta2564@users.noreply.github.com>
2025-09-22 16:18:04 +0530

    Create 398-RandomPickIndex

---
+++ 398-RandomPickIndex/398-RandomPickIndex.java
@@ -0,0 +1,26 @@
+// Last updated: 22/09/2025, 16:18:02
+import java.util.*;
+
+class Solution {
+    private int[] nums;
+    private Random rand;
+
+    public Solution(int[] nums) {
+        this.nums = nums;
+        this.rand = new Random();
+    }
+
+    public int pick(int target) {
+        int count = 0;
+        int result = -1;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == target) {
+                count++;
+                if (rand.nextInt(count) == 0) {
+                    result = i;
+                }
+            }
+        }
+        return result;
+    }
+}
