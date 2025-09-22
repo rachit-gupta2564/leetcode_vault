31352070e0148078c80f5e8d358672db21f4497e
Rachit Gupta <160449023+rachit-gupta2564@users.noreply.github.com>
2025-09-22 16:18:53 +0530

    Create 349-IntersectionOfTwoArrays

---
+++ 349-IntersectionOfTwoArrays/349-IntersectionOfTwoArrays.java
@@ -0,0 +1,19 @@
+// Last updated: 22/09/2025, 16:18:51
+import java.util.*;
+
+class Solution {
+    public int[] intersection(int[] nums1, int[] nums2) {
+        Set<Integer> set1 = new HashSet<>();
+        for (int num : nums1) set1.add(num);
+        
+        Set<Integer> resultSet = new HashSet<>();
+        for (int num : nums2) {
+            if (set1.contains(num)) resultSet.add(num);
+        }
+        
+        int[] result = new int[resultSet.size()];
+        int i = 0;
+        for (int num : resultSet) result[i++] = num;
+        return result;
+    }
+}
