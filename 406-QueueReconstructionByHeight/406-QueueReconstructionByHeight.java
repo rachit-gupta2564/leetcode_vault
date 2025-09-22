97225b7097bea349e7fee5170e1be49366c9555b
Rachit Gupta <160449023+rachit-gupta2564@users.noreply.github.com>
2025-09-22 16:17:54 +0530

    Create 406-QueueReconstructionByHeight

---
+++ 406-QueueReconstructionByHeight/406-QueueReconstructionByHeight.java
@@ -0,0 +1,11 @@
+// Last updated: 22/09/2025, 16:17:52
+import java.util.*;
+
+class Solution {
+    public int[][] reconstructQueue(int[][] people) {
+        Arrays.sort(people, (a, b) -> a[0] == b[0] ? a[1] - b[1] : b[0] - a[0]);
+        List<int[]> ans = new LinkedList<>();
+        for (int[] p : people) ans.add(p[1], p);
+        return ans.toArray(new int[people.length][]);
+    }
+}
