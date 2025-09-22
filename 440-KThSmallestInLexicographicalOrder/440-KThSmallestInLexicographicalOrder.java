0df23e42401c973cb242f316bd8447b13d8c3ecd
Rachit Gupta <160449023+rachit-gupta2564@users.noreply.github.com>
2025-09-22 16:17:24 +0530

    Create 440-KThSmallestInLexicographicalOrder

---
+++ 440-KThSmallestInLexicographicalOrder/440-KThSmallestInLexicographicalOrder.java
@@ -0,0 +1,28 @@
+// Last updated: 22/09/2025, 16:17:22
+class Solution {
+    public int findKthNumber(int n, int k) {
+        int curr = 1;
+        k--;
+        while (k > 0) {
+            long steps = getSteps(n, curr, curr + 1);
+            if (steps <= k) {
+                k -= steps;
+                curr++;
+            } else {
+                curr *= 10;
+                k--;
+            }
+        }
+        return curr;
+    }
+    
+    private long getSteps(int n, long curr, long next) {
+        long steps = 0;
+        while (curr <= n) {
+            steps += Math.min(n + 1L, next) - curr;
+            curr *= 10;
+            next *= 10;
+        }
+        return steps;
+    }
+}
