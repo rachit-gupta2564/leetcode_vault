0e42f4fe5a394e42dc47d26168a6ac0b7ae03744
Rachit Gupta <160449023+rachit-gupta2564@users.noreply.github.com>
2025-09-22 16:19:16 +0530

    Create 335-SelfCrossing

---
+++ 335-SelfCrossing/335-SelfCrossing.java
@@ -0,0 +1,12 @@
+// Last updated: 22/09/2025, 16:19:14
+class Solution {
+    public boolean isSelfCrossing(int[] x) {
+        for (int i = 3; i < x.length; i++) {
+            if (x[i] >= x[i - 2] && x[i - 1] <= x[i - 3]) return true;
+            if (i >= 4 && x[i - 1] == x[i - 3] && x[i] + x[i - 4] >= x[i - 2]) return true;
+            if (i >= 5 && x[i - 2] >= x[i - 4] && x[i] + x[i - 4] >= x[i - 2]
+                && x[i - 1] <= x[i - 3] && x[i - 1] + x[i - 5] >= x[i - 3]) return true;
+        }
+        return false;
+    }
+}
