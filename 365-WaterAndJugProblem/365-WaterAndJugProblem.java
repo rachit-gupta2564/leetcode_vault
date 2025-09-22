a8e440d0caec7c7bb8f93b427968de357440ef3b
Rachit Gupta <160449023+rachit-gupta2564@users.noreply.github.com>
2025-09-22 16:18:44 +0530

    Create 365-WaterAndJugProblem

---
+++ 365-WaterAndJugProblem/365-WaterAndJugProblem.java
@@ -0,0 +1,17 @@
+// Last updated: 22/09/2025, 16:18:42
+class Solution {
+    public boolean canMeasureWater(int x, int y, int target) {
+        if (target == 0) return true;
+        if (x + y < target) return false;
+        return target % gcd(x, y) == 0;
+    }
+
+    private int gcd(int a, int b) {
+        while (b != 0) {
+            int t = a % b;
+            a = b;
+            b = t;
+        }
+        return a;
+    }
+}
