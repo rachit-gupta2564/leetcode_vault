2a9c51da133aeb805120bcc003369605a6ee81f1
Rachit Gupta <160449023+rachit-gupta2564@users.noreply.github.com>
2025-09-22 16:14:52 +0530

    Create 937-OnlineStockSpan

---
+++ 937-OnlineStockSpan/937-OnlineStockSpan.java
@@ -0,0 +1,26 @@
+// Last updated: 22/09/2025, 16:14:50
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+class StockSpanner {
+    private Deque<int[]> stack; // pair: [price, span]
+
+    public StockSpanner() {
+        stack = new ArrayDeque<>();
+    }
+
+    public int next(int price) {
+        int span = 1;
+        while (!stack.isEmpty() && stack.peek()[0] <= price) {
+            span += stack.pop()[1];
+        }
+        stack.push(new int[]{price, span});
+        return span;
+    }
+}
+
+/**
+ * Your StockSpanner object will be instantiated and called as such:
+ * StockSpanner obj = new StockSpanner();
+ * int param_1 = obj.next(price);
+ */
