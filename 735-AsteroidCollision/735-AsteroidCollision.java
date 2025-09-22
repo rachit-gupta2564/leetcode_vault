4d0df8b087279b7c8fae136a5e97e136af44f7c8
Rachit Gupta <160449023+rachit-gupta2564@users.noreply.github.com>
2025-09-22 16:15:48 +0530

    Create 735-AsteroidCollision

---
+++ 735-AsteroidCollision/735-AsteroidCollision.java
@@ -0,0 +1,26 @@
+// Last updated: 22/09/2025, 16:15:46
+class Solution {
+    public int[] asteroidCollision(int[] asteroids) {
+        Stack<Integer> stack = new Stack<>();
+        for (int ast : asteroids) {
+            boolean destroyed = false;
+            while (!stack.isEmpty() && ast < 0 && stack.peek() > 0) {
+                if (stack.peek() < -ast) {
+                    stack.pop();
+                    continue;
+                } else if (stack.peek() == -ast) {
+                    stack.pop();
+                }
+                destroyed = true;
+                break;
+            }
+            if (!destroyed) stack.push(ast);
+        }
+        
+        int[] result = new int[stack.size()];
+        for (int i = stack.size() - 1; i >= 0; i--) {
+            result[i] = stack.pop();
+        }
+        return result;
+    }
+}
