21217df3880f0cc92df71db6c543c5baa3db9436
Rachit Gupta <160449023+rachit-gupta2564@users.noreply.github.com>
2025-09-22 16:15:37 +0530

    Create 790-GlobalAndLocalInversions

---
+++ 790-GlobalAndLocalInversions/790-GlobalAndLocalInversions.java
@@ -0,0 +1,14 @@
+// Last updated: 22/09/2025, 16:15:35
+class Solution {
+    public boolean isIdealPermutation(int[] nums) {
+        int n = nums.length;
+        int maxSoFar = -1;
+        for (int i = 0; i < n - 2; i++) {
+            maxSoFar = Math.max(maxSoFar, nums[i]);
+            if (maxSoFar > nums[i + 2]) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
