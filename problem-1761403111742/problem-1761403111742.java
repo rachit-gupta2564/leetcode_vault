e306119b15098fbf26a98877b31fc0395ae5406d
Rachit Gupta <160449023+rachit-gupta2564@users.noreply.github.com>
2025-10-25 20:08:34 +0530

    Create

---
+++ problem-1761403111742/problem-1761403111742.java
@@ -0,0 +1,33 @@
+// Last updated: 25/10/2025, 20:08:31
+class Solution {
+    public String lexSmallest(String s) {
+        char[] arr = s.toCharArray();
+        String best = s;
+        int n = arr.length;
+        for (int k = 1; k <= n; k++) {
+            char[] first = new char[n];
+            for (int i = 0, j = k - 1; i < k; i++, j--) {
+                first[i] = arr[j];
+            }
+            for (int i = k; i < n; i++) {
+                first[i] = arr[i];
+            }
+            String s1 = new String(first);
+            if (s1.compareTo(best) < 0) {
+                best = s1;
+            }
+            char[] last = new char[n];
+            for (int i = n - k, j = n - 1; j >= n - k; i++, j--) {
+                last[i] = arr[j];
+            }
+            for (int i = 0; i < n - k; i++) {
+                last[i] = arr[i];
+            }
+            String s2 = new String(last);
+            if (s2.compareTo(best) < 0) {
+                best = s2;
+            }
+        }
+        return best;
+    }
+}
