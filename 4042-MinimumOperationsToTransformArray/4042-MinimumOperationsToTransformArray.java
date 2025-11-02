12f7c8bb635f5fae030ef357ecf681efeb789c0d
Rachit Gupta <160449023+rachit-gupta2564@users.noreply.github.com>
2025-11-02 23:39:20 +0530

    Create 4042-MinimumOperationsToTransformArray

---
+++ 4042-MinimumOperationsToTransformArray/4042-MinimumOperationsToTransformArray.java
@@ -0,0 +1,22 @@
+// Last updated: 02/11/2025, 23:39:20
+class Solution {
+    public long minOperations(int[] nums1, int[] nums2) {
+        int n = nums1.length;
+        long s = 0;
+        for (int i = 0; i < n; i++) {
+            s += Math.abs((long)nums2[i] - nums1[i]);
+        }
+        long extra = Long.MAX_VALUE;
+        long t = nums2[n];
+        for (int i = 0; i < n; i++) {
+            long a = nums1[i], b = nums2[i];
+            long f1 = Math.abs(b - a) + Math.abs(t - a);
+            long f2 = Math.abs(a - b) + Math.abs(t - b);
+            long f3 = Math.abs(a - t) + Math.abs(b - t);
+            long bestHere = Math.min(f1, Math.min(f2, f3));
+            long change = bestHere - Math.abs(b - a);
+            if (change < extra) extra = change;
+        }
+        return s + extra + 1;
+    }
+}
