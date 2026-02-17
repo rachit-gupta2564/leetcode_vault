a79b2469e5ee5a6195ce367a53110efc2f830159
Rachit Gupta <160449023+rachit-gupta2564@users.noreply.github.com>
2026-02-17 22:36:46 +0530

    File updated at 17/02/2026, 22:36:47

--- 401-BinaryWatch/401-BinaryWatch.java
+++ 401-BinaryWatch/401-BinaryWatch.java
@@ -1,16 +1,16 @@
-// Last updated: 22/09/2025, 07:47:46
-import java.util.*;
-
-class Solution {
-    public List<String> readBinaryWatch(int turnedOn) {
-        List<String> res = new ArrayList<>();
-        for (int h = 0; h < 12; h++) {
-            for (int m = 0; m < 60; m++) {
-                if (Integer.bitCount(h) + Integer.bitCount(m) == turnedOn) {
-                    res.add(h + ":" + (m < 10 ? "0" + m : m));
-                }
-            }
-        }
-        return res;
-    }
-}
+// Last updated: 17/02/2026, 22:36:47
+1import java.util.*;
+2
+3class Solution {
+4    public List<String> readBinaryWatch(int turnedOn) {
+5        List<String> res = new ArrayList<>();
+6        for (int h = 0; h < 12; h++) {
+7            for (int m = 0; m < 60; m++) {
+8                if (Integer.bitCount(h) + Integer.bitCount(m) == turnedOn) {
+9                    res.add(h + ":" + (m < 10 ? "0" + m : m));
+10                }
+11            }
+12        }
+13        return res;
+14    }
+15}
